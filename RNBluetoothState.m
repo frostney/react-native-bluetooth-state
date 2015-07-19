55c780bf68b2791b6766aa56d065f101c11b1f25
Johannes Stein <johannesstein@freeze-dev.com>
2015-07-19 20:22:38 +0200

    Native code

---
+++ RNBluetoothState.m
@@ -0,0 +1,55 @@
+#import <CoreBluetooth/CoreBluetooth.h>
+
+#import "RNBluetoothState.h"
+#import "RCTEventDispatcher.h"
+
+@interface RNBluetoothState() <CBCentralManagerDelegate>
+
+@property(strong, nonatomic) CBCentralManager *centralManager;
+
+@end
+
+@implementation RNBluetoothState
+
+RCT_EXPORT_MODULE()
+
+#pragma mark Initialization
+
+- (instancetype)init
+{
+    if (self = [super init]) {
+        centralManager = [[CBCentralManager alloc] initWithDelegate:self queue:dispatch_get_main_queue()];
+    }
+    
+    return self;
+}
+
+- (NSString *) centralManagerStateToString: (int)state
+{
+    switch (state) {
+        case CBCentralManagerStateUnknown:
+            return @"unknown";
+        case CBCentralManagerStateResetting:
+            return @"resetting";
+        case CBCentralManagerStateUnsupported:
+            return @"unsupported";
+        case CBCentralManagerStateUnauthorized:
+            return @"unauthorized";
+        case CBCentralManagerStatePoweredOff:
+            return @"off";
+        case CBCentralManagerStatePoweredOn:
+            return @"on";
+        default:
+            return @"unknown";
+    }
+    
+    return @"unknown";
+}
+
+- (void)centralManagerDidUpdateState:(CBCentralManager *)central
+{
+    NSString *stateName = [self centralManagerStateToString:central.state];
+    [self.bridge.eventDispatcher sendDeviceEventWithName:@"centralManagerDidUpdateState" body:stateName];
+}
+
+@end
